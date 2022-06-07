4d63c4c3ec2d6cef5b3020d7cbf057090b7e53e8
Elektron97 <danele.caradonna@gmail.com>
2022-06-07 23:26:42 +0200

    taskLib completata

--- Filters/taskLib.c
+++ Filters/taskLib.c
@@ -54,3 +54,66 @@ void set_activation(int idx)
     time_add_ms(&(param[idx].actTime), param[idx].period);
     time_add_ms(&(param[idx].deadline), param[idx].period);
 }
+
+/*This function makes a task idx sleep and resets it for next period*/
+void wait_for_activation(int idx)
+{
+    //sleeps until activation time
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &(param[idx].actTime), NULL);
+
+    //resets next activation time and deadline
+    time_add_ms(&(param[idx].actTime), param[idx].period);
+    time_add_ms(&(param[idx].deadline), param[idx].period);
+}
+
+/*wait for task: implementing pthread_join in idx system*/
+void wait_for_task(int idx)
+{
+    pthread_join(tid[idx], NULL);
+    return;
+}
+
+/*Deadline Miss*/
+int deadline_miss(int idx)
+{
+    struct timespec t;
+
+    //read current time
+    clock_gettime(CLOCK_MONOTONIC, &t);
+
+    //check
+    if(time_cmp(t, param[idx].deadline) > 0)
+    {
+        param[idx].dMiss++; //increases deadline miss counter
+        return 1;
+    }
+
+    return 0;
+}
+
+/*Get Task Index*/
+int get_task_index(void* arg)
+{
+    struct task_par *tpar;
+
+    tpar = (struct task_par *)arg;
+    return tpar->arg; //returns task index (arg)
+}
+
+/*frand generates a random number in a range*/
+float frand(float xmi, float xma)
+{
+    float r;
+
+    r = rand()/(float)RAND_MAX;
+    return xmi + (xma - xmi)*r;
+}
+
+/*Get scan code: fetches a pressed key as char*/
+char get_scancode()
+{
+    if(keypressed())
+        return readkey() >> 8;
+    else
+        return 0;
+}
--- Filters/taskLib.h
+++ Filters/taskLib.h
@@ -19,11 +19,11 @@ pthread_t tid[TMAX] ;
 
 int	task_create(void*	(*task)(void	*), int	i, int	period, int	drel, int	prio);
 void set_activation(int	i);
-void wait_for_activation(int	i);
-void wait_for_task(int	i);
-int	deadline_miss(int	i);
-int	get_task_index(void*	arg);
-float frand(float	xmi, float	xma);
+void wait_for_activation(int i);
+void wait_for_task(int i);
+int	deadline_miss(int i);
+int	get_task_index(void* arg);
+float frand(float xmi, float xma);
 char get_scancode();
 
 #endif /* TASKLIB_H_ */
