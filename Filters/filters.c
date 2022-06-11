f6e88c8673cc30aae226b243fc20cf6a6359b14a
Elektron97 <danele.caradonna@gmail.com>
2022-06-11 12:00:15 +0200

    Add black screen with allegro

--- Filters/filters.c
+++ Filters/filters.c
@@ -17,19 +17,32 @@
 #include "taskLib.h"
 
 /*DEFINE*/
+//task parameters
 #define PER 40
 #define PRIO 1
 #define IDX 1
+
+//graphics parameters
+#define WIDTH 800
+#define HEIGHT 600
+
+//colours
+#define BLACK 0
+#define YELLOW 14
 
 /*GLOBAL VARIABLES*/
 int end_flag = 0;
 
 /*FUNCTION SIGNATURES*/
+void init();
 void *helloWorldTask(void* arg);
+void *waveTask(void* arg);
 
 /*MAIN*/
 int main(void)
 {
+    init();
+
     int task_report; // 0: Ok | 1: Task Issues
 
     printf("Ciao!\n");
@@ -37,7 +50,22 @@ int main(void)
     printf("Hello World Task! Report: %d \n", task_report);
     wait_for_task(IDX);
 
+    allegro_exit();
     return 0;
+}
+
+void init()
+{
+    allegro_init();
+    install_keyboard();
+
+    set_color_depth(8); //256 VGA
+
+    //Enter in Graphics mode with full screen(width, height)
+	set_gfx_mode(GFX_AUTODETECT_WINDOWED, WIDTH, HEIGHT, 0, 0);
+
+	//Clear Screen: ("global BITMAP called screen")
+	clear_to_color(screen, BLACK); //Black Background
 }
 
 /*FUNCTIONS*/
@@ -54,6 +82,26 @@ void *helloWorldTask(void* arg)
         
         if(deadline_miss(idx))
             printf("******Deadline Miss!******** \n");
+
+        wait_for_activation(idx);
+    }
+
+    return NULL;
+}
+
+void *waveTask(void* arg)
+{
+    int idx = get_task_index(arg);
+    set_activation(idx);
+
+    while(!end_flag)
+    {
+        /************************
+         * Body of Task here:   *
+         * **********************/
+        
+        if(deadline_miss(idx))
+            printf("******Deadline Miss of Wave Task!******** \n");
 
         wait_for_activation(idx);
     }
