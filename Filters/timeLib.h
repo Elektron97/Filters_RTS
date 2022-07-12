d543e927d1a31bd0e40d379e8997383b5ff981ab
Elektron97 <danele.caradonna@gmail.com>
2022-07-12 22:59:09 +0200

    to do list

--- Filters/timeLib.h
+++
@@ -1,8 +0,0 @@
-#ifndef TLIB_H_
-#define TLIB_H_
-
-void time_copy(struct timespec *td, struct timespec ts);
-void time_add_ms(struct timespec *t, int ms);
-int time_cmp(struct timespec t1, struct timespec t2);
-
-#endif /* TLIB_H_ */
